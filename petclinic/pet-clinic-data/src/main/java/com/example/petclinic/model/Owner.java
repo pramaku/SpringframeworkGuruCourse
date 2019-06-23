0ed8bce287d5355903cbc9ddf274068de46552e5
PradeepKumar <pandillapradeepkumar@gmail.com>
2019-06-23 19:35:28 +0530

    Adding Visit entity. Closes #26

--- petclinic/pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
+++ petclinic/pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
@@ -1,6 +1,19 @@
 package com.example.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Owner extends Person
 {
-	
+	Set<Pet> pets = new HashSet<>();
+
+	public Set<Pet> getPets()
+	{
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets)
+	{
+		this.pets = pets;
+	}
 }
