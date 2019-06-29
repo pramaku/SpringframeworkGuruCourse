d3c6bcc308f99ae2b0f98683ea846e6a040cdc73
PradeepKumar <pandillapradeepkumar@gmail.com>
2019-06-29 18:30:33 +0530

    Adding base entity. Closes #33

--- petclinic/pet-clinic-data/src/main/java/com/example/petclinic/model/BaseEntity.java
+++ petclinic/pet-clinic-data/src/main/java/com/example/petclinic/model/BaseEntity.java
@@ -2,8 +2,17 @@ package com.example.petclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@SuppressWarnings("serial")
+@MappedSuperclass
 public class BaseEntity implements Serializable
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId()
